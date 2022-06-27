10290cea58832d21d0f9af6e64843101cfe09a30
Aulia Mahardika <aulia.mahardika@live.com>
2022-06-27 22:31:41 +0700

    update progressing disqus embed

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -15,6 +15,7 @@ import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import Layout, { GradientBackground } from '../../components/Layout';
 import SEO from '../../components/SEO';
+import {DiscussionEmbed} from 'disqus-react';
 import { CodePen } from 'mdx-embed';// Custom components/renderers to pass to MDX.
 // Since the MDX files aren't loaded by webpack, they have no knowledge of how
 // to handle import statements. Instead, you must include components in scope
@@ -27,6 +28,10 @@ const components = {
   Head,
   CodePen,
 };
+
+const DisqusComm = ({ post }) => {
+  const disqusShortname = ''
+}
 
 export default function PostPage({
   source,
